cc21d0d091f50e1f1cdba7b421576fb808a7fbd2
Rodrigo Moguillansky <romoguill@gmail.com>
2023-05-09 14:19:36 -0300

    Created new folder for desafio3 and moved classes to their own files

---
+++ desafio3/ProductManager.js
@@ -0,0 +1,108 @@
+export class ProductManager {
+  constructor(dbPath = __dirname) {
+    this.path = path.join(dbPath, 'database-ProductManager.json');
+    this.#createDB();
+  }
+
+  #createDB() {
+    fs.promises
+      .writeFile(this.path, JSON.stringify([]))
+      .then(() => console.log('Database has been initialized'))
+      .catch(() => console.log('Error creating the database'));
+  }
+
+  #readFile() {
+    return fs.promises.readFile(this.path);
+  }
+
+  #writeFile(data) {
+    return fs.promises.writeFile(this.path, data);
+  }
+
+  #getLastId(products) {
+    const ids = products.map((product) => product.id);
+    return Math.max(ids);
+  }
+
+  #isCodeDuplicate(products, code) {
+    return products.find((product) => product.code === code) === undefined
+      ? false
+      : true;
+  }
+
+  async getProducts() {
+    try {
+      const data = await this.#readFile();
+      return JSON.parse(data);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async addProduct(product) {
+    if (!(product instanceof Product)) {
+      console.log('Fail to add. Arg must be of type Product');
+      return;
+    }
+
+    try {
+      const products = await this.getProducts();
+
+      if (this.#isCodeDuplicate(products, product.code)) {
+        throw new Error('Fail to add product. Duplicate code property');
+      }
+
+      const newProductId = this.#getLastId(products) + 1;
+      products.push({ id: newProductId, ...product });
+
+      await this.#writeFile(JSON.stringify(products));
+    } catch (error) {
+      console.log("Couldn't add product");
+      throw error;
+    }
+  }
+
+  async getProductById(id) {
+    try {
+      const products = await this.getProducts();
+      const product = products.find((product) => product.id === id);
+
+      if (product === undefined) {
+        throw new Error('No product found with that id');
+      }
+
+      return product;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateProduct(id, updatedValues) {
+    try {
+      const products = await this.getProducts();
+
+      const productIndex = products.findIndex((product) => product.id === id);
+      products[productIndex] = { ...products[productIndex], ...updatedValues };
+
+      this.#writeFile(JSON.stringify(products));
+
+      return products[productIndex];
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteProduct(id) {
+    try {
+      await this.getProductById(id);
+
+      let products = await this.getProducts();
+
+      products = products.filter((product) => product.id !== id);
+
+      this.#writeFile(JSON.stringify(products));
+    } catch (error) {
+      throw error;
+    }
+  }
+}
